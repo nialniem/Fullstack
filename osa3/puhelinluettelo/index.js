85a0244b6e3a77ec496de350ce38c1fda57f81dc
Niklas Nieminen <niklas.niaminen@gmail.com>
2026-01-07 18:59:17 +0200

    puhelinluettelo osa 3
    tein tehtävät 3.6 asti. postmanilla testattu että kaikki toimii.

---
+++ osa3/puhelinluettelo/index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "040-123456"
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "39-23-6423122"
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "12-43-234345"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+  })
+
+  app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+  })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+  })
+
+  app.get('/api/info', (req, res) => {
+    const count = persons.length
+    const date = new Date()
+  
+    res.send(`
+      <p>Phonebook has info for ${count} people</p>
+      <p>${date}</p>
+    `)
+  })
+
+  const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => Number(n.id)))
+      : 0
+    return String(maxId + 1)
+  }
+  
+  app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+    const nameExists = persons.some(person => person.name === body.name)
+
+    if (nameExists) {
+        return response.status(400).json({
+        error: 'name must be unique'
+    })
+    }
+  
+    const person = {
+      id: generateId(),
+      name: body.name,
+      number: body.number || false,
+      
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+  })
+
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
