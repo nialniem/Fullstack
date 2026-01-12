6301691915e44cd4e979919f41c9103d6e90170b
Niklas Nieminen <niklas.niaminen@gmail.com>
2026-01-12 13:30:25 +0200

    blogilista
    4.1 valmis

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+const mongoUrl = process.env.MONGODB_URI
+console.log('MONGODB_URI is', mongoUrl ? 'set' : 'missing')
+
+mongoose
+  .connect(mongoUrl, { family: 4 })
+  .then(() => console.log('connected to MongoDB, db =', mongoose.connection.name))
+  .catch((err) => console.log('Mongo connection error:', err.message))
+
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = process.env.PORT || 3003
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
