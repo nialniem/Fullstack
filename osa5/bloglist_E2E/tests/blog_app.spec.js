2b6604eadd5dcb7854d60a617083c5163f1d7709
Niklas Nieminen <niklas.niaminen@gmail.com>
2026-01-16 14:04:07 +0200

    blogilistan end to end step2
    testit kirjautumiselle

--- osa5/bloglist_E2E/tests/blog_app.spec.js
+++ osa5/bloglist_E2E/tests/blog_app.spec.js
@@ -1,6 +1,6 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
-const BACKEND = 'http://127.0.0.1:3003'
+const BACKEND = 'http://127.0.0.1:3003' 
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -21,4 +21,29 @@ describe('Blog app', () => {
     await expect(page.getByText('log in to application')).toBeVisible()
     await expect(page.getByRole('button', { name: /login/i })).toBeVisible()
   })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByRole('textbox').first().fill('mluukkai')
+      await page.locator('input[type="password"]').fill('salainen')
+      await page.getByRole('button', { name: /login/i }).click()
+  
+      await expect(page.getByText(/logged in/i)).toBeVisible()
+  
+      await expect(page.getByRole('button', { name: /login/i })).not.toBeVisible()
+    })
+  
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByRole('textbox').first().fill('mluukkai')
+      await page.locator('input[type="password"]').fill('väärä')
+      await page.getByRole('button', { name: /login/i }).click()
+  
+      await expect(page.getByText(/wrong|invalid/i)).toBeVisible()
+
+      await expect(page.getByRole('button', { name: /login/i })).toBeVisible()
+  
+      await expect(page.getByText(/logged in/i)).not.toBeVisible()
+    })
+  })
+  
 })
